2bf825fe4eb0db0a9cab34e64cffb02f439145ae
Sebastian Michaelsen <sebastian@michaelsen.io>
2023-12-04 12:36:21 +0100

    release: 2.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,18 @@
 <?php
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'App Routes',
     'description' => 'Easy way to route rest-like URLs to your code',
     'category' => 'frontend',
     'author' => 'Sebastian Michaelsen',
     'author_email' => 'sebastian@michaelsen.io',
+    'author_company' => '',
     'state' => 'stable',
-    'version' => '1.4.2',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.99.99',
         ],
+        'conflicts' => [],
+        'suggests' => []
     ],
 ];
