52da7d2dd48468b3cef6610f432e9bf788e99546
Sebastian Michaelsen <sebastian@michaelsen.io>
2026-02-16 12:04:00 +0100

    fix: make RouteViewHelper compatible with TYPO3 v14

--- Classes/ViewHelpers/RouteViewHelper.php
+++ Classes/ViewHelpers/RouteViewHelper.php
@@ -5,21 +5,24 @@ namespace Sinso\AppRoutes\ViewHelpers;
 
 use Sinso\AppRoutes\Service\Router;
 use Symfony\Component\Routing\Generator\UrlGenerator;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class RouteViewHelper extends AbstractViewHelper
 {
-    public function initializeArguments()
+    public function __construct(private readonly Router $router) {}
+
+    public function initializeArguments(): void
     {
         $this->registerArgument('routeName', 'string', '', true);
         $this->registerArgument('parameters', 'array', '', false, []);
     }
 
-    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    public function render(): string
     {
-        $router = GeneralUtility::makeInstance(Router::class);
-        return $router->getUrlGenerator()->generate($arguments['routeName'], $arguments['parameters'], UrlGenerator::ABSOLUTE_URL);
+        return $this->router->getUrlGenerator()->generate(
+            $this->arguments['routeName'],
+            $this->arguments['parameters'],
+            UrlGenerator::ABSOLUTE_URL,
+        );
     }
 }
